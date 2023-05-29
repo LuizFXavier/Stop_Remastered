55d7ccfaf29ce5100e6fd3d0e235f8eebcb0db1f
LuizFXavier <zap422238@gmail.com>
2023-05-29 00:00:22 -0400

    proporção alterada

--- public/TS/main.ts
+++ public/TS/main.ts
@@ -4,10 +4,8 @@ import Input from "./UI/Input";
 const canvas = document.getElementById('canvas1') as HTMLCanvasElement;
 Games.ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const CANVAS_WIDTH = canvas.width = window.innerWidth * 0.99;
-const CANVAS_HEIGHT = canvas.height = window.innerHeight * 0.99;
-const CANVAS_X = canvas.getBoundingClientRect().x
-const CANVAS_Y = canvas.getBoundingClientRect().y
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('click', (e) => {
     
